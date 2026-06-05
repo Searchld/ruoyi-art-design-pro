81497c97cc8662b7b6c1bbd7d7f87f7926677ec6
Searchld <13188888456@qq.com>
2026-06-05 15:48:54 +0800

    Delete sql directory
    群内获取sql

--- sql/20260531_add_sys_dept_code.sql
+++
@@ -1,6 +0,0 @@
-alter table sys_dept
-  add column dept_code varchar(64) not null default '' comment '部门编码' after dept_name;
-
-update sys_dept
-set dept_code = concat('DEPT_', dept_id)
-where dept_code is null or dept_code = '';
--- sql/20260601_default_art_design_pro_generator.sql
+++
@@ -1,5 +0,0 @@
--- 新导入的代码生成业务表默认使用 Art Design Pro 模板。
--- 不更新已有 gen_table 记录，保留历史记录原有模板选择。
-alter table gen_table
-  modify column tpl_web_type varchar(30) default 'art-design-pro'
-  comment '前端模板类型（Art Design Pro、element-ui、element-plus模版）';
